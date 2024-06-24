3310a8e8c2a3e65a4933bec734e47d803f619b9a
Akimov Roman <frbhjvf@yandex.ru>
2024-06-24 18:40:21 +0300

    Фикс зависимостей и настроек проекта

---
+++ src/main/java/ru/web_registers/web_registers/controllers/MainController.java
@@ -0,0 +1,12 @@
+package ru.web_registers.web_registers.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String login() {
+        return "login";
+    }
+}

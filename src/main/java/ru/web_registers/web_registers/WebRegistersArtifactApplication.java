c826b1042e0f686ff0ea4502026853d4d943c72f
Akimov Roman <frbhjvf@yandex.ru>
2024-06-24 11:17:12 +0300

    Инициализация проекта
    Создание проекта

---
+++ src/main/java/ru/web_registers/web_registers/WebRegistersArtifactApplication.java
@@ -0,0 +1,13 @@
+package ru.web_registers.web_registers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRegistersArtifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebRegistersArtifactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/web_registers/web_registers/WebRegistersArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.web_registers.web_registers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebRegistersArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

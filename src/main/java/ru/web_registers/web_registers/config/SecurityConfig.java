13298cdcb709fe9a18a306ce816cccd6a30b3fe1
Akimov Roman <frbhjvf@yandex.ru>
2024-06-25 10:19:39 +0300

    Настройка кастомного входа Spring Security

--- src/main/java/ru/web_registers/web_registers/config/SecurityConfig.java
+++ src/main/java/ru/web_registers/web_registers/config/SecurityConfig.java
@@ -1,22 +1,20 @@
 package ru.web_registers.web_registers.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
 public class SecurityConfig {
 
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .permitAll()
-                );
-        return http.build();
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/main", true).permitAll())
+                .authorizeHttpRequests(auth->auth.anyRequest().authenticated())
+                .build();
     }
 }
